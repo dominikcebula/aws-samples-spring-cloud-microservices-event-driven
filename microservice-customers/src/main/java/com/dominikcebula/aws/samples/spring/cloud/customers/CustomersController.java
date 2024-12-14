8fcaf38d487315d3083c785cc058205218a2d838
Dominik Cebula <dominikcebula@gmail.com>
2024-12-14 21:45:23 +0100

    aws-samples-spring-cloud-microservices-event-driven

---
+++ microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/CustomersController.java
@@ -0,0 +1,14 @@
+package com.dominikcebula.aws.samples.spring.cloud.customers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CustomersController {
+    @GetMapping("/customers")
+    public String getCustomers() {
+        return "OK";
+    }
+}
