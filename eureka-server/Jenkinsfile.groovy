f53718e188a4ab70130cf65aece79bd00dab6a2d
Dominik Cebula <dominikcebula@gmail.com>
2024-12-24 00:18:04 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- eureka-server/Jenkinsfile.groovy
+++ eureka-server/Jenkinsfile.groovy
@@ -1,7 +1,10 @@
 podTemplate(agentContainer: 'maven', agentInjection: true, containers: [
-        containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21'),
+        containerTemplate(
+                name: 'maven', image: 'maven:3.9-eclipse-temurin-21',
+                volumes: [genericEphemeralVolume(accessModes: 'ReadWriteOnce', mountPath: '/root/.m2/repository', requestsSize: '1Gi')]
+        ),
         containerTemplate(name: 'kaniko', image: "gcr.io/kaniko-project/executor:debug", command: '/busybox/cat', ttyEnabled: true)
-], volumes: [genericEphemeralVolume(accessModes: 'ReadWriteOnce', mountPath: '/root/.m2/repository', requestsSize: '1Gi')]) {
+]) {
     node(POD_LABEL) {
         environment {
             AWS_REGION = 'eu-central-1'
