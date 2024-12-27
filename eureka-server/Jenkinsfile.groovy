33c9f1d2338e7eb878b7970ce564c6c492577dd7
Dominik Cebula <dominikcebula@gmail.com>
2024-12-27 21:08:33 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- eureka-server/Jenkinsfile.groovy
+++ eureka-server/Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 podTemplate(agentContainer: 'maven', agentInjection: true, containers: [
         containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21'),
         containerTemplate(name: 'kaniko', image: "gcr.io/kaniko-project/executor:debug", command: '/busybox/cat', ttyEnabled: true)
-], volumes: [genericEphemeralVolume(claimName: 'maven-repo', mountPath: '/root/.m2/repository')]) {
+], volumes: [persistentVolumeClaim(claimName: 'maven-repo', mountPath: '/root/.m2/repository')]) {
     node(POD_LABEL) {
         stage('Checkout') {
             echo 'Checking out source code...'
