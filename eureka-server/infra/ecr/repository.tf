a9dd83e784b6f8e374807add76e3408fd1da86d3
Dominik Cebula <dominikcebula@gmail.com>
2024-12-28 15:53:42 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- eureka-server/infra/ecr/repository.tf
+++ eureka-server/infra/ecr/repository.tf
@@ -1,3 +1,4 @@
 resource "aws_ecr_repository" "eureka_server" {
-  name = "aws-samples-spring-cloud-microservices-event-driven/eureka-server"
+  name         = "aws-samples-spring-cloud-microservices-event-driven/eureka-server"
+  force_delete = true
 }
