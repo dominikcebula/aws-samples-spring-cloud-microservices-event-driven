d6905c7c5ec920cb1d6a2b9e6a537ec66b41a402
Dominik Cebula <dominikcebula@gmail.com>
2024-12-30 17:41:49 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- cicd/infra/eks/eks.tf
+++ cicd/infra/eks/eks.tf
@@ -36,9 +36,9 @@ module "eks" {
 
       instance_types = ["t3.medium"]
 
-      min_size     = 1
-      max_size     = 1
-      desired_size = 1
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
     }
   }
 }
