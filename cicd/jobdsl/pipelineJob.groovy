9bb75900412cf54f68d051e2f5882bc84ee80ad3
Dominik Cebula <dominikcebula@gmail.com>
2024-12-30 12:00:08 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- cicd/jobdsl/pipelineJob.groovy
+++ cicd/jobdsl/pipelineJob.groovy
@@ -1,1 +1,21 @@
 def test = 1
+
+def definePipelineJob() {
+    pipelineJob('eureka-server') {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url('https://github.com/dominikcebula/aws-samples-spring-cloud-microservices-event-driven.git')
+                            credentials('jenkins-github-token')
+                        }
+                        branch('*/main')
+                    }
+                }
+                scriptPath('eureka-server/Jenkinsfile.groovy')
+                lightweight()
+            }
+        }
+    }
+}
