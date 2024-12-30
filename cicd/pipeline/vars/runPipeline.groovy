c64137117c9c5caab6c4f66f0308d9083df29996
Dominik Cebula <dominikcebula@gmail.com>
2024-12-30 10:55:45 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- cicd/pipeline/vars/runPipeline.groovy
+++ cicd/pipeline/vars/runPipeline.groovy
@@ -10,48 +10,54 @@ def call(Map pipelineParams) {
             emptyDirVolume(mountPath: '/.kube')
     ]) {
         node(POD_LABEL) {
-            stage('Checkout') {
-                echo 'Checking out source code...'
-                checkout scm
-            }
-
-            stage('Build') {
-                echo 'Building the project...'
-                sh 'mvn clean compile'
-            }
-
-            stage('Unit Test') {
-                echo 'Running unit tests...'
-                sh 'mvn test'
-            }
-
-            stage('Integration Test') {
-                echo 'Running integration tests...'
-                sh 'mvn verify'
-            }
-
-            stage('Package') {
-                echo 'Packaging the application...'
-                sh 'mvn package'
-            }
-
-            stage('Containerize') {
-                container(name: 'kaniko', shell: '/busybox/sh') {
-                    echo 'Building and uploading docker image using kaniko...'
-                    def ecrImageUrl = "${ECR_REPO_NAMESPACE_URL}/${pipelineParams.serviceName}:latest"
-                    sh "/kaniko/executor --dockerfile Dockerfile --context `pwd`/eureka-server --destination ${ecrImageUrl}"
-                }
-            }
-
-            stage('Configure Kubernetes Client') {
-                container(name: 'awscli') {
-                    sh "aws eks update-kubeconfig --name ${AWS_EKS_CLUSTER_NAME} --region ${AWS_REGION} --kubeconfig /.kube/config"
-                }
-            }
+//            stage('Checkout') {
+//                echo 'Checking out source code...'
+//                checkout scm
+//            }
+//
+//            stage('Build') {
+//                echo 'Building the project...'
+//                sh 'mvn clean compile'
+//            }
+//
+//            stage('Unit Test') {
+//                echo 'Running unit tests...'
+//                sh 'mvn test'
+//            }
+//
+//            stage('Integration Test') {
+//                echo 'Running integration tests...'
+//                sh 'mvn verify'
+//            }
+//
+//            stage('Package') {
+//                echo 'Packaging the application...'
+//                sh 'mvn package'
+//            }
+//
+//            stage('Containerize') {
+//                container(name: 'kaniko', shell: '/busybox/sh') {
+//                    echo 'Building and uploading docker image using kaniko...'
+//                    def ecrImageUrl = "${ECR_REPO_NAMESPACE_URL}/${pipelineParams.serviceName}:latest"
+//                    sh "/kaniko/executor --dockerfile Dockerfile --context `pwd`/eureka-server --destination ${ecrImageUrl}"
+//                }
+//            }
+//
+//            stage('Configure Kubernetes Client') {
+//                container(name: 'awscli') {
+//                    sh "aws eks update-kubeconfig --name ${AWS_EKS_CLUSTER_NAME} --region ${AWS_REGION} --kubeconfig /.kube/config"
+//                }
+//            }
 
             stage('Deploy') {
                 container(name: 'kubectl') {
-                    sh "for file in `pwd`/${pipelineParams.serviceName}/deployment/*.yaml; do envsubst < \$file | KUBECONFIG=/.kube/config kubectl apply -f -; done"
+                    sh """
+                         for file in \$(pwd)/${pipelineParams.serviceName}/deployment/*.yaml; do \
+                             echo "Applying \$(basename \${file})..."; \
+                             envsubst < \${file} | KUBECONFIG=/.kube/config kubectl apply -f -; \
+                             echo "Finished applying Applying \$(basename \${file})...\n"; \
+                         done
+                        """
                 }
             }
         }
