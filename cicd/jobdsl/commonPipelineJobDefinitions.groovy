32275e59004c78253b711ce208b9515432cab31c
Dominik Cebula <dominikcebula@gmail.com>
2024-12-30 12:34:52 +0100

    aws-samples-spring-cloud-microservices-event-driven

---
+++ cicd/jobdsl/commonPipelineJobDefinitions.groovy
@@ -0,0 +1,5 @@
+GIT_URL = 'https://github.com/dominikcebula/aws-samples-spring-cloud-microservices-event-driven.git'
+GIT_BRANCH = '*/main'
+GIT_CREDENTIALS_ID = 'jenkins-github-token'
+
+PIPELINE_SCRIPT_NAME = 'Jenkinsfile.groovy'
