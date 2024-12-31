aded86af4ff9b642bf1f43528371ce4ba8e0b71b
Dominik Cebula <dominikcebula@gmail.com>
2024-12-31 15:48:29 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/RdsIamDataSourceConfiguration.java
+++ microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/RdsIamDataSourceConfiguration.java
@@ -1,10 +1,12 @@
 package com.dominikcebula.aws.samples.spring.cloud.customers;
 
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
@@ -15,34 +17,47 @@ import java.sql.SQLException;
 
 @Configuration
 @Profile("aws")
+@Log
 public class RdsIamDataSourceConfiguration {
     @Value("${spring.datasource.url}")
-    private String dbUrl;
+    private String jdbcUrl;
 
     @Value("${spring.datasource.username}")
     private String username;
 
     @Bean
     public DataSource dataSource() throws SQLException {
+        log.info("Creating RDS IAM DataSource");
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(dbUrl);
+        dataSource.setUrl(jdbcUrl);
         dataSource.setUsername(username);
         dataSource.setPassword(generateAuthToken());
+
+        log.info("RDS IAM URL: " + dataSource.getUrl());
+        log.info("RDS IAM Username: " + dataSource.getUsername());
+        log.info("RDS IAM Password: " + dataSource.getPassword());
+
+        log.info("RDS IAM DataSource created");
 
         return dataSource;
     }
 
     private String generateAuthToken() {
+        Region region = new DefaultAwsRegionProviderChain().getRegion();
+
+        log.info("Generating RDS IAM token for region: " + region);
+
         try (RdsClient rdsClient = RdsClient.builder()
-                .region(new DefaultAwsRegionProviderChain().getRegion())
+                .region(region)
                 .build()) {
-            URI jdbcUri = parseJdbcURL(dbUrl);
+            URI dbUri = parseJdbcURL(jdbcUrl);
 
             GenerateAuthenticationTokenRequest request = GenerateAuthenticationTokenRequest.builder()
                     .username(username)
-                    .hostname(jdbcUri.getHost())
-                    .port(jdbcUri.getPort())
+                    .hostname(dbUri.getHost())
+                    .port(dbUri.getPort())
                     .build();
 
             return rdsClient.utilities()
