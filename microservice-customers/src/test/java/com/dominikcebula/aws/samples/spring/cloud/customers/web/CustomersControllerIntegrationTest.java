45db412508c1be02854c9ced8dc9b20b493a68b4
Dominik Cebula <dominikcebula@gmail.com>
2025-01-09 22:52:00 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- microservice-customers/src/test/java/com/dominikcebula/aws/samples/spring/cloud/customers/web/CustomersControllerIntegrationTest.java
+++ microservice-customers/src/test/java/com/dominikcebula/aws/samples/spring/cloud/customers/web/CustomersControllerIntegrationTest.java
@@ -1,11 +1,26 @@
 package com.dominikcebula.aws.samples.spring.cloud.customers.web;
 
+import com.dominikcebula.aws.samples.spring.cloud.customers.model.CustomerDTO;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @Testcontainers
@@ -15,7 +30,37 @@ class CustomersControllerIntegrationTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Container
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:17");
+
     @Test
     void shouldReturnEmptyCustomersList() {
+        // when
+        ResponseEntity<List<CustomerDTO>> responseEntity = restTemplate.exchange(getCustomers(), HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        // then
+        assertThat(responseEntity.getStatusCode())
+                .isEqualTo(HttpStatus.OK);
+    }
+
+    private @NotNull String getCustomers() {
+        return getBaseUrl() + "/customers";
+    }
+
+    private String getBaseUrl() {
+        return "http://localhost:" + port + "/api/v1";
+    }
+
+    @DynamicPropertySource
+    private static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
     }
 }
