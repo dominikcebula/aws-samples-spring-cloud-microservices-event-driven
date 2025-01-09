c9d6657ada9e42cbc2f935215872a1e0f22f8a83
Dominik Cebula <dominikcebula@gmail.com>
2025-01-09 22:01:10 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/service/CustomerService.java
+++ microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/service/CustomerService.java
@@ -2,9 +2,13 @@ package com.dominikcebula.aws.samples.spring.cloud.customers.service;
 
 import com.dominikcebula.aws.samples.spring.cloud.customers.model.CustomerDTO;
 import com.dominikcebula.aws.samples.spring.cloud.customers.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CustomerService {
     @Autowired
@@ -14,24 +18,46 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
-    public CustomerDTO getCustomerById(Long id) {
-        return customerRepository.findById(id).orElse(null);
+    public Optional<CustomerDTO> getCustomerById(Long id) {
+        return customerRepository.findById(id);
     }
-
 
     public CustomerDTO createCustomer(CustomerDTO customer) {
         return customerRepository.save(customer);
     }
 
-    public CustomerDTO updateCustomer(Long id, CustomerDTO customer) {
+    public UpdateCustomerResultData updateCustomer(Long id, CustomerDTO customer) {
         if (customerRepository.existsById(id)) {
             customer.setId(id);
-            return customerRepository.save(customer);
-        }
-        return null;
+            CustomerDTO savedCustomer = customerRepository.save(customer);
+            return new UpdateCustomerResultData(UpdateCustomerResult.UPDATED, savedCustomer);
+        } else
+            return new UpdateCustomerResultData(UpdateCustomerResult.NOT_FOUND);
     }
 
-    public void deleteCustomer(Long id) {
-        customerRepository.deleteById(id);
+    public DeleteCustomerResult deleteCustomer(Long id) {
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+            return DeleteCustomerResult.DELETED;
+        } else
+            return DeleteCustomerResult.NOT_FOUND;
+    }
+
+    @RequiredArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class UpdateCustomerResultData {
+        private final UpdateCustomerResult updateCustomerResult;
+        private CustomerDTO savedCustomer;
+    }
+
+    public enum UpdateCustomerResult {
+        UPDATED,
+        NOT_FOUND
+    }
+
+    public enum DeleteCustomerResult {
+        DELETED,
+        NOT_FOUND
     }
 }
--- microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/web/CustomersController.java
+++ microservice-customers/src/main/java/com/dominikcebula/aws/samples/spring/cloud/customers/web/CustomersController.java
@@ -2,12 +2,17 @@ package com.dominikcebula.aws.samples.spring.cloud.customers.web;
 
 import com.dominikcebula.aws.samples.spring.cloud.customers.model.CustomerDTO;
 import com.dominikcebula.aws.samples.spring.cloud.customers.service.CustomerService;
+import com.dominikcebula.aws.samples.spring.cloud.customers.service.CustomerService.UpdateCustomerResultData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.dominikcebula.aws.samples.spring.cloud.customers.service.CustomerService.DeleteCustomerResult;
+import static com.dominikcebula.aws.samples.spring.cloud.customers.service.CustomerService.UpdateCustomerResult;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,37 +27,37 @@ public class CustomersController {
 
     @GetMapping("/customers/{id}")
     public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id) {
-        CustomerDTO customer = customerService.getCustomerById(id);
-        if (customer != null) {
-            return ResponseEntity.ok(customer);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return customerService.getCustomerById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping("/customers")
     public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customer) {
         CustomerDTO createdCustomer = customerService.createCustomer(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header(HttpHeaders.LOCATION, "/api/v1/customers/" + createdCustomer.getId())
+                .body(createdCustomer);
     }
 
     @PutMapping("/customers/{id}")
     public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customer) {
-        CustomerDTO updatedCustomer = customerService.updateCustomer(id, customer);
-        if (updatedCustomer != null) {
-            return ResponseEntity.ok(updatedCustomer);
-        } else {
+        UpdateCustomerResultData updateCustomerResultData = customerService.updateCustomer(id, customer);
+
+        if (updateCustomerResultData.getUpdateCustomerResult() == UpdateCustomerResult.UPDATED)
+            return ResponseEntity.ok().body(updateCustomerResultData.getSavedCustomer());
+        else
             return ResponseEntity.notFound().build();
-        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
-        if (customerService.getCustomerById(id) != null) {
-            customerService.deleteCustomer(id);
+        DeleteCustomerResult deleteCustomerResult = customerService.deleteCustomer(id);
+
+        if (deleteCustomerResult == DeleteCustomerResult.DELETED)
             return ResponseEntity.noContent().build();
-        } else {
+        else
             return ResponseEntity.notFound().build();
-        }
     }
 }
