3141e764bb79f16605e60cd5ea012ed893dbff20
Dominik Cebula <dominikcebula@gmail.com>
2025-02-01 00:51:27 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- microservice-shipment/infra/sqs/queue.tf
+++ microservice-shipment/infra/sqs/queue.tf
@@ -7,3 +7,47 @@ resource "aws_sns_topic_subscription" "customer_events_to_shipment_service_subsc
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.customer_events_to_shipment_service.arn
 }
+
+resource "aws_sqs_queue_policy" "sqs_policy" {
+  queue_url = aws_sqs_queue.customer_events_to_shipment_service.url
+  policy    = data.aws_iam_policy_document.sqs_policy_document.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "sqs_policy_document" {
+  statement {
+    sid = "__owner_statement"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = [data.aws_caller_identity.current.arn]
+      type = "AWS"
+    }
+
+    actions = ["SQS:*"]
+    resources = [aws_sqs_queue.customer_events_to_shipment_service.arn]
+  }
+
+  statement {
+    sid = "topic-subscription-arn:${var.customer_events_topic_arn}"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+
+    actions = ["SQS:SendMessage"]
+
+    resources = [aws_sqs_queue.customer_events_to_shipment_service.arn]
+
+    condition {
+      test     = "ArnLike"
+      values = [var.customer_events_topic_arn]
+      variable = "aws:SourceArn"
+    }
+  }
+}
