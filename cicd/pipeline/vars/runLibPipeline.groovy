8ec23e58a2eb049b616d228866d0aae9c3139086
Dominik Cebula <dominikcebula@gmail.com>
2025-02-21 20:29:32 +0100

    aws-samples-spring-cloud-microservices-event-driven

--- cicd/pipeline/vars/runLibPipeline.groovy
+++ cicd/pipeline/vars/runLibPipeline.groovy
@@ -45,9 +45,6 @@ def call(Map pipelineParams) {
             stage('Deploy') {
                 echo 'Deploying library...'
 
-                sh "env"
-                sh "cat /root/.m2/settings.xml"
-                sh "mvn help:effective-settings -f ${WORKSPACE}/${pipelineParams.libName}/pom.xml"
                 sh "mvn -f ${WORKSPACE}/${pipelineParams.libName}/pom.xml deploy"
             }
         }
